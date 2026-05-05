14762f5acd54d50d5ee4ef8eaabbdebcafd59358
Ifhdhd <108957265+Ifhdhd@users.noreply.github.com>
2026-05-05 16:57:39 +0700

    Refactor login function and add buildHeaders

--- services/loginService.js
+++ services/loginService.js
@@ -2,10 +2,54 @@
 
 const axios = require("axios");
 
+/*
+|--------------------------------------------------------------------------
+| BUILD HEADERS
+|--------------------------------------------------------------------------
+*/
+
+function buildHeaders(cookie = "") {
+
+  return {
+
+    "Content-Type": "application/json",
+
+    "X-COUNTRY-ID": "1",
+    countryCode: "ID",
+    timeZoneId: "Asia/Jakarta",
+    country: "ID",
+
+    "Accept-Language": "in-ID",
+
+    deviceId:
+      "ffffffff-a665-1a66-0000-0000748ca5f0",
+
+    deviceModel: "5030U",
+
+    osVersion: "10",
+
+    versionCode: "122",
+    versionName: "2.9.2-release",
+
+    "User-Agent":
+      "okhttp/4.9.2",
+
+    Cookie: cookie
+
+  };
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| LOGIN
+|--------------------------------------------------------------------------
+*/
+
 async function login({
-  username,
-  password,
-  type
+  account,
+  pwd,
+  appVersion = "1"
 }) {
 
   try {
@@ -18,68 +62,59 @@ async function login({
 
     const response =
       await axios.post(
-        "https://ez-co-app.tin.group/app/login",
+
+        "https://ez-co-app.tin.group/app/offline/user/login",
+
         {
 
-          /*
-          |------------------------------------------------------------------
-          | USERNAME
-          |------------------------------------------------------------------
-          */
-
-          account: username,
-
-          /*
-          |------------------------------------------------------------------
-          | PASSWORD
-          |------------------------------------------------------------------
-          | PASSWORD SUDAH MD5 DARI auth.js
-          */
-
-          password: password,
-
-          /*
-          |------------------------------------------------------------------
-          | TYPE
-          |------------------------------------------------------------------
-          | 1 = zizhangyi
-          | 0 = non zizhangyi
-          */
-
-          type: type || 0
+          account,
+          pwd,
+          appVersion
 
         },
+
         {
 
-          headers: {
-
-            "X-COUNTRY-ID": "1",
-            countryCode: "ID",
-            timeZoneId: "Asia/Jakarta",
-            country: "ID",
-            "Accept-Language": "in-ID",
-            deviceId:
-              "ffffffff-a665-1a66-0000-0000748ca5f0",
-            deviceModel: "5030U",
-            osVersion: "10",
-            versionCode: "122",
-            versionName: "2.9.2-release",
-            "User-Agent":
-              "okhttp/4.9.2"
-
-          }
+          headers:
+            buildHeaders()
 
         }
+
       );
 
     /*
     |--------------------------------------------------------------------------
-    | COOKIE
+    | COOKIES
     |--------------------------------------------------------------------------
     */
 
-    const cookies =
+    const rawCookies =
       response.headers["set-cookie"] || [];
+
+    let SESSION = "";
+    let acw_tc = "";
+
+    rawCookies.forEach(cookie => {
+
+      if(cookie.includes("SESSION=")){
+
+        SESSION =
+          cookie
+            .split("SESSION=")[1]
+            .split(";")[0];
+
+      }
+
+      if(cookie.includes("acw_tc=")){
+
+        acw_tc =
+          cookie
+            .split("acw_tc=")[1]
+            .split(";")[0];
+
+      }
+
+    });
 
     /*
     |--------------------------------------------------------------------------
@@ -91,12 +126,16 @@ async function login({
 
       success: true,
 
-      data: {
+      data:
+        response.data.data,
 
-        user:
-          response.data,
+      raw:
+        response.data,
 
-        cookies
+      cookies: {
+
+        SESSION,
+        acw_tc
 
       }
 
@@ -104,18 +143,18 @@ async function login({
 
   } catch (err) {
 
-    /*
-    |--------------------------------------------------------------------------
-    | FAILED
-    |--------------------------------------------------------------------------
-    */
+    console.log(
+      "LOGIN ERROR:",
+      err.response?.data ||
+      err.message
+    );
 
     return {
 
       success: false,
 
       message:
-        err?.response?.data?.message ||
+        err.response?.data ||
         err.message
 
     };
@@ -125,5 +164,6 @@ async function login({
 }
 
 module.exports = {
-  login
+  login,
+  buildHeaders
 };
